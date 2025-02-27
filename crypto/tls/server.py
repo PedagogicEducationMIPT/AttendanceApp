78f8160a4a9b44188600b452c255a25ab2110247
root <root@WIN-1J6F5NF0R04>
2025-02-27 03:04:13 +0300

    tls

---
+++ crypto/tls/server.py
@@ -0,0 +1,15 @@
+import uvicorn
+import fastapi
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def hello():
+    return 'hello'
+
+uvicorn.run(
+    app,
+    ssl_certfile='certificate.crt',
+    ssl_keyfile='private.key'
+)
